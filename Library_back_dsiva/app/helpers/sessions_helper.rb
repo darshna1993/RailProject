0e7ea0a234833bbcd63d9abd997e9417343e0a6c
Darshna Siva <dsiva@ncsu.edu>
2016-09-25 20:28:07 -0400

    Rails Project for Heroku upload

---
+++ Library_back_dsiva/app/helpers/sessions_helper.rb
@@ -0,0 +1,34 @@
+module SessionsHelper
+
+
+def log_in user  
+  session[:user_id] = user.id
+end
+
+def current_user
+   if @current_user.nil?
+      @current_user = User.find_by(id: session[:user_id])
+   else
+      @current_user
+   end      
+end
+
+def logged_in?
+   !@current_user.nil?
+   	
+end
+
+def log_out
+   session.delete(:user_id)
+   @current_user = nil
+end   
+end
+
+def is_admin? user
+
+	if user.admin
+		
+	end
+end
+
+# The concepts of creating session was referenced from https://www.railstutorial.org/book/basic_login . The concept of sessions and html being a 'tasteless' 
